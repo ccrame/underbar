56bcf3cf59d833b3d2e87b5cbba1a7cc9870b7b6
Charles Crame <ccrame@gmail.com>
2015-05-23 21:23:15 -0700

    Completed 'reject'

--- src/underbar.js
+++ src/underbar.js
@@ -93,6 +93,10 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+	var result = _.filter(collection,function(item){
+	  return !test(item);
+	});
+	return result;
   };
 
   // Produce a duplicate-free version of the array.

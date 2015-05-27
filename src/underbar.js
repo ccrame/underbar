59d205b67002b0697caafd3b1899f9e6c810b4f7
Charles Crame <ccrame@gmail.com>
2015-05-27 02:53:24 -0700

    Completed 'extend'

--- src/underbar.js
+++ src/underbar.js
@@ -224,6 +224,12 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.each(arguments,function(arg){
+	  _.each(arg,function(item,key){
+	    obj[key] = item;
+	  });
+	});
+	return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
